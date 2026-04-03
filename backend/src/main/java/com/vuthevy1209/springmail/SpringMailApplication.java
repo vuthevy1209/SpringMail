f27afba7392326763ad009f2347e0f729f47704f
Vũ Thế Vỹ <vuthevy12092004@gmail.com>
2026-04-03 14:37:47 +0700

    init: backend soure with spring web, spring data mongodb, Oauth2 Client

---
+++ backend/src/main/java/com/vuthevy1209/springmail/SpringMailApplication.java
@@ -0,0 +1,13 @@
+package com.vuthevy1209.springmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vuthevy1209/springmail/SpringMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuthevy1209.springmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
