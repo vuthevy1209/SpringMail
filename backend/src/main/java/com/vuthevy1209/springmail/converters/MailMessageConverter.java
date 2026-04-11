c13c7fd8b6a16fe5a4c72264b671af0cc4fdf9ae
Vũ Thế Vỹ <vuthevy12092004@gmail.com>
2026-04-11 17:51:44 +0700

    fix: display image error with c:id

--- backend/src/main/java/com/vuthevy1209/springmail/converters/MailMessageConverter.java
+++ backend/src/main/java/com/vuthevy1209/springmail/converters/MailMessageConverter.java
@@ -1,9 +1,12 @@
 package com.vuthevy1209.springmail.converters;
 
 import com.vuthevy1209.springmail.dto.mail.response.MailMessageResponse;
+import com.vuthevy1209.springmail.dto.mail.response.MessageAttachmentResponse;
 import com.vuthevy1209.springmail.entity.MailMessage;
 import com.vuthevy1209.springmail.enums.MailLabel;
+import com.vuthevy1209.springmail.service.gmail.dto.attachment.GmailAttachmentDto;
 import com.vuthevy1209.springmail.service.gmail.dto.message.GmailMessageDto;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -15,6 +18,12 @@ import java.util.List;
 public class MailMessageConverter {
 
 	private final ModelMapper modelMapper;
+
+	@PostConstruct
+	public void init() {
+		modelMapper.typeMap(GmailAttachmentDto.class, MessageAttachmentResponse.class)
+				.addMapping(GmailAttachmentDto::getAttachmentId, MessageAttachmentResponse::setId);
+	}
 
 	public MailMessage toMailMessage(GmailMessageDto dto) {
 		return modelMapper.map(dto, MailMessage.class);
